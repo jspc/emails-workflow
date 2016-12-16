18ff1b267fe13fa657e627301aa7a02757ed9ed5
jspc <james@zero-internet.org.uk>
2016-12-16 12:18:43 +0000

    gofmt

--- jobs.go
+++ jobs.go
@@ -1,25 +1,31 @@
 package main
 
 import (
-    "net/http"
+	"encoding/base64"
+	"fmt"
+	"net/mail"
+	"net/smtp"
+	"os"
 
-    "github.com/gincorp/gin/taskmanager"
-    owm "github.com/briandowns/openweathermap"
+	owm "github.com/briandowns/openweathermap"
+	"github.com/gincorp/gin/taskmanager"
 )
 
 func getLondonWeather(jn taskmanager.JobNotification) (output map[string]interface{}, err error) {
-    w, err := owm.NewForecast("C", "en")
-    if err != nil {
-        return
-    }
+	w, err := owm.NewForecast("C", "en")
+	if err != nil {
+		return
+	}
 
-    w.DailyByName("London,UK",1)
-    day := w.List[0]
+	location := jn.Context["location"]
+	w.DailyByName(location, 1)
+	day := w.List[0]
 
-    output["minimum"] = day.Temp.Min
-    output["maximum"] = day.Temp.Max
+	output = make(map[string]interface{})
+	output["minimum"] = day.Temp.Min
+	output["maximum"] = day.Temp.Max
 
-    return
+	return
 }
 
 func sendEmail(jn taskmanager.JobNotification) (output map[string]interface{}, err error) {
